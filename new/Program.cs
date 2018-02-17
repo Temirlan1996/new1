67f92b4c70a0db40af808d12d27c0ea3b7df6b02
Persival <temirlan.kuralbaev@mail.ru>
2018-02-17 23:44:57 +0600

    Добавьте файлы проекта.

---
+++ new/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @new
+{
+    class Program
+    {
+        struct Group 
+            {
+            public int kurs;
+            public string Name;
+            }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Group g = new Group();
+            List<Group> gr = new List<Group>();
+            string s = "tima";
+            try
+            {
+                int a = s;
+            }catch(Exception e)
+            {
+                Console.WriteLine("error " + e);
+            }
+
+            Console.Read();
+        }
+    }
+}
